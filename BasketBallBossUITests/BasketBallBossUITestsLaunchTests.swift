b57a1ae8914a60df1da81671358a09e81bdf9b90
noelortiz <noelo12130613@gmail.com>
2023-11-13 10:00:15 -0600

    Initial Commit

---
+++ BasketBallBossUITests/BasketBallBossUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BasketBallBossUITestsLaunchTests.swift
+//  BasketBallBossUITests
+//
+//  Created by Noel Ortiz on 11/13/23.
+//
+
+import XCTest
+
+final class BasketBallBossUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}

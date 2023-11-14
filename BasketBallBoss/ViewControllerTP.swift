0f24d608cba3188b6ffc01819f2261b4a8cad1a4
noelortiz <noelo12130613@gmail.com>
2023-11-14 09:09:58 -0600

    inital commit

---
+++ BasketBallBoss/ViewControllerTP.swift
@@ -0,0 +1,29 @@
+//
+//  ViewControllerTP.swift
+//  BasketBallBoss
+//
+//  Created by Noel Ortiz on 11/13/23.
+//
+
+import UIKit
+
+class ViewControllerTP: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
